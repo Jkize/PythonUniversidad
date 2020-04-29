e7a471d625a4151266ce521d23d1e6847e0d27bd
Jhose <sebastiansaavedra.1998@hotmail.com>
2020-04-29 01:32:09 -0500

    avances

--- Procesamiento de Lenguaje/Data/sentlex-master/push.sh
+++
@@ -1,8 +0,0 @@
-echo "Building"
-rm dist/*gz
-python setup.py sdist
-pip install --upgrade --force-reinstall --no-deps dist/*gz
-
-echo "Testing"
-cd tests
-py.test *.py
--- Procesamiento de Lenguaje/Data/sentlex-master/sentlex/__init__.py
+++
@@ -1,8 +0,0 @@
-'''
-  sentlex package __init__
-'''
-# bring up the goods into main namespace
-from __future__ import absolute_import
-from .sentlex import *
-from .sentanalysis import *
-from .stopwords import *
